e86ce9efc398e9679482465da15f211ee195103e
Baptiste Salaud <baptiste.salaud@gmail.com>
2023-12-05 17:58:49 +0100

    feat: :zap: home view

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:blindtest/views/HomeView.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        fontFamily: 'LilitaOne',
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff86E4DF))
+            .copyWith(background: const Color(0xff86E4DF)),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xff86E4DF),
+          foregroundColor: Color(0xff0C173E),
+        ),
+      ),
+      home: const HomeView(),
+    );
+  }
+}
---
+++ lib/views/HomeView.dart
@@ -0,0 +1,68 @@
+
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => HomeViewState();
+}
+
+class HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(leading : IconButton(
+            icon: const Icon(Icons.menu, size: 60),
+            onPressed: () {},
+          )
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(bottom: 20),
+                child: const Text(
+                  'BLINDTEST',
+                  style: TextStyle(
+                      fontFamily: 'LilitaOne',
+                      fontSize: 60,
+                      color: Color(0xff33AD97)),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 20),
+                child: SizedBox(
+                    width: 250,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              const Color(0xff0C173E)), // background
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                              side: const BorderSide(
+                                  width: 3, color: Colors.black),
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          'PLAY',
+                          style: TextStyle(color: Colors.white, fontSize: 35),
+                        ))),
+              ),
+              Container(
+                  width: 400,
+                  alignment: Alignment.centerRight,
+                  child: SizedBox(
+                      width: 150,
+                      child: Image.asset(
+                        'assets/images/click.gif',
+                      )))
+            ],
+          ),
+        ));
+  }
+}
